1cd8191265d66b0955e64f55ac185d967a97907d
lwp <251701981@qq.com>
2018-07-20 11:14:10 +0800

    firends template

--- pages/firend/index.js
+++ pages/firend/index.js
@@ -1,10 +1,7 @@
 var Vue = require('/modules/vue.js');
 var mui = require('/modules/mui.js');
-mui.plusReady(function(){
-	plus.nativeUI.showWaiting('Loading...');
-	setTimeout(function(){
-		plus.webview.show(plus.webview.currentWebview().id);
-		plus.nativeUI.closeWaiting();
-	},1000);
-
+var app = require('/pages/firend/index.vue');
+new Vue({
+	el:'#app',
+	components:{app:app}
 });
--- pages/firend/index.vue
+++ pages/firend/index.vue
@@ -1,92 +1,34 @@
 <template>
 	<div>
-		<ul class="mui-table-view mui-table-view-striped mui-table-view-condensed">
-			<li class="mui-table-view-cell">
-				<div class="mui-slider-cell">
-					<div class="oa-contact-cell mui-table">
-						<div class="oa-contact-avatar mui-table-cell">
-							<img src="../images/60x60.gif" />
+		<ul class="mui-table-view firends">
+			<li class="mui-table-view-cell firends-item">
+			    <div class="firends-item-left" >
+				    <img src="/static/imgs/60X60.gif" />
+				</div>
+				<div class="firends-item-mid" >
+				    <div class="firends-item-mid-up" ><h4>昵称</h4></div>
+					<div class="firends-item-mid-dow" >
+					    <div class="firends-item-mid-dow-state" >
+						    <h5>[在线]</h5>
 						</div>
-						<div class="oa-contact-content mui-table-cell">
-							<div class="mui-clearfix">
-								<h4 class="oa-contact-name">叶文洁</h4>
-								<span class="oa-contact-position mui-h6">董事长</span>
-							</div>
-							<p class="oa-contact-email mui-h6">
-								yewenjie@sina.com
-							</p>
+						<div class="firends-item-mid-dow-sign" >
+						    签名.....签名签名签名签名签名签名签名签名签名
 						</div>
 					</div>
 				</div>
 			</li>
-			<li class="mui-table-view-cell">
-				<div class="mui-slider-cell">
-					<div class="oa-contact-cell mui-table">
-						<div class="oa-contact-avatar mui-table-cell">
-							<img src="../images/60x60.gif" />
-						</div>
-						<div class="oa-contact-content mui-table-cell">
-							<div class="mui-clearfix">
-								<h4 class="oa-contact-name">艾AA</h4>
-								<span class="oa-contact-position mui-h6">总经理</span>
-							</div>
-							<p class="oa-contact-email mui-h6">
-								aaa@163.com
-							</p>
-						</div>
-					</div>
+						<li class="mui-table-view-cell firends-item">
+			    <div class="firends-item-left" >
+				    <img src="/static/imgs/60X60.gif" />
 				</div>
-			</li>
-			<li class="mui-table-view-cell">
-				<div class="mui-slider-cell">
-					<div class="oa-contact-cell mui-table">
-						<div class="oa-contact-avatar mui-table-cell">
-							<img src="../images/60x60.gif" />
+				<div class="firends-item-mid" >
+				    <div class="firends-item-mid-up" ><h4>昵称</h4></div>
+					<div class="firends-item-mid-dow" >
+					    <div class="firends-item-mid-dow-state" >
+						    <h5>[在线]</h5>
 						</div>
-						<div class="oa-contact-content mui-table-cell">
-							<div class="mui-clearfix">
-								<h4 class="oa-contact-name">罗辑</h4>
-								<span class="oa-contact-position mui-h6">员工</span>
-							</div>
-							<p class="oa-contact-email mui-h6">
-								luoji@126.com
-							</p>
-						</div>
-					</div>
-				</div>
-			</li>
-			<li class="mui-table-view-cell">
-				<div class="mui-slider-cell">
-					<div class="oa-contact-cell mui-table">
-						<div class="oa-contact-avatar mui-table-cell">
-							<img src="../images/60x60.gif" />
-						</div>
-						<div class="oa-contact-content mui-table-cell">
-							<div class="mui-clearfix">
-								<h4 class="oa-contact-name">云天明</h4>
-								<span class="oa-contact-position mui-h6">员工</span>
-							</div>
-							<p class="oa-contact-email mui-h6">
-								ytm@163.com
-							</p>
-						</div>
-					</div>
-				</div>
-			</li>
-			<li class="mui-table-view-cell">
-				<div class="mui-slider-cell">
-					<div class="oa-contact-cell mui-table">
-						<div class="oa-contact-avatar mui-table-cell">
-							<img src="../images/60x60.gif" />
-						</div>
-						<div class="oa-contact-content mui-table-cell">
-							<div class="mui-clearfix">
-								<h4 class="oa-contact-name">史强</h4>
-								<span class="oa-contact-position mui-h6">员工</span>
-							</div>
-							<p class="oa-contact-email mui-h6">
-								shiqiang@gmail.com
-							</p>
+						<div class="firends-item-mid-dow-sign" >
+						    签名.....签名签名签名签名签名签名签名签名签名
 						</div>
 					</div>
 				</div>
@@ -96,8 +38,15 @@
 </template>
 
 <script>
+    module.exports = {};
 </script>
 
 <style scoped >
-
+    .firends-item-left{float:left;width:1.4rem;height:1.4rem;}
+	.firends-item-left img{width:1.4rem;height:1.4rem;display:block;border-radius:50%;}
+	.firends-item-mid{float:left;width:7.4rem;height:1.4rem;padding-left:0.1rem;}
+	.firends-item-mid-up{text-overflow:ellipsis;overflow:hidden;white-space:nowrap;}
+	.firends-item-mid-dow-state,.firends-item-mid-dow-sign{float:left;}
+	.firends-item-mid-dow-sign{padding-left:0.1rem;text-overflow:ellipsis;overflow:hidden;width:6rem;height:0.7rem;white-space:nowrap;}
+	.firends-item-mid-dow,.firends-item-mid-up{line-height:0.7rem;}
 </style>
